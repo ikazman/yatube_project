a9061d098c1ceaeff6db44524a9e79773f24e766
Aleksey Petukhov <ikazman@yandex.ru>
2021-04-20 18:03:05 +0300

    PEP8 tweaks

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
+
 def index(request):
-# одна строка вместо тысячи слов на SQL
     latest = Post.objects.order_by('-pub_date')[:10]
-    # собираем тексты постов в один, разделяя новой строкой
     output = []
     for item in latest:
         output.append(item.text)
