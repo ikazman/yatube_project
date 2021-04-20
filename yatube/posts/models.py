d528fd60e3282ff8c5c7e9c7e9d16fca703df02a
Aleksey Petukhov <ikazman@yandex.ru>
2021-04-20 13:19:27 +0300

    Rename "pub" to "pub_date"

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,6 +6,6 @@ User = get_user_model()
 
 class Post(models.Model):
     text = models.TextField()
-    pub = models.DateTimeField('date published', auto_now_add=True)
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='posts')
