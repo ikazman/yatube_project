362da4796b9fac9f136090f10ab794bb9c009ce8
Aleksey Petukhov <ikazman@yandex.ru>
2021-04-22 11:26:25 +0300

    Added templates and static

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,8 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Post
 
 
 def index(request):
-    latest = Post.objects.order_by('-pub_date')[:10]
-    output = []
-    for item in latest:
-        output.append(item.text)
-    return HttpResponse('\n'.join(output))
+    latest = Post.objects.order_by('-pub_date')[:11]
+    return render(request, 'index.html', {'posts': latest})
